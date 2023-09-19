835ac16d3365092abedf8f5da9789092460114a5
gablon29 <gabriellondero40@gmail.com>
2023-09-19 17:21:32 -0300

    testeo de las rutas exitoso

--- src/main/java/com/example/crudRapido/controllers/EstudianteController.java
+++ src/main/java/com/example/crudRapido/controllers/EstudianteController.java
@@ -3,7 +3,8 @@ package com.example.crudRapido.controllers;
 import com.example.crudRapido.entity.Estudiante;
 import com.example.crudRapido.service.EstudianteService;
 import java.util.*;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+ // yo quisiera hacer una peticion que me devuelva un string por consola que me diga peticion realizada correctamente como lo harias?
 
 @RestController
 @RequestMapping(path = "api/v1/estudiantes")
@@ -21,8 +22,11 @@ public class EstudianteController {
     @Autowired
     EstudianteService estudianteService;
 
+    private final Logger logger = LoggerFactory.getLogger(EstudianteController.class);
+
     @GetMapping
     public List<Estudiante> getAll() {
+        logger.info("Peticion Get realizada con exito");
         return estudianteService.getEstudiantes();
     }
     
